2ab19bf9165c664e32c8727166f03f3dcd2cbabe
wangzheng62 <363579018@qq.com>
2019-04-22 16:15:51 +0800

    11

--- SSHtool/test.py
+++ SSHtool/test.py
@@ -1,26 +1,21 @@
 import os,sys
 from paramiko import AutoAddPolicy,Transport,Channel,client,SSHClient
 from time import sleep
-sys.path.append(os.path.abspath('..'))
-print(sys.path)
-from SSHtool.interactive import windows_shell
-#transport
-tran=Transport(('192.168.220.129',22))
-tran.connect(username='wz',password='12345678')
-#client
-UBUNTU01={'hostname':'192.168.0.123'}
-client01=SSHClient()
-client01.set_missing_host_key_policy(AutoAddPolicy())
-client01.connect(hostname='192.168.220.129',port=22,username='wz',password='12345678')
-chan=client01.invoke_shell()
-windows_shell(chan)
-print(chan.__str__())
-#chan.setblocking(1)
-#chan.settimeout(2.0)
-#chan.send('sudo apt -y -f remove sslscan'+'\n'+'12345678'+'\n')
-#chan.send('pwd'+'\n')
-#chan.send('ping 127.0.0.1 \n')
-#sleep(10)
-#inf=chan.recv(20000)
-#print(inf.decode())
-client01.close()
+import socket
+socket01=socket.socket()
+socket01.connect(('192.168.220.129',22))
+tran=Transport(socket01)
+tran.connect(username='root',password='123456789')
+print(tran.is_active())
+chan=tran.open_channel(kind='session')
+chan.invoke_shell()
+com='sudo apt -y -f install sslscan \n' \
+        '12345678\n'
+com1='pwd\n'
+com2='apt-get -y -f install sslscan\n'
+chan.send(com2)
+sleep(5)
+if chan.recv_stderr_ready():
+    print(chan.recv_stderr(10000).decode())
+else:
+    print(chan.recv(10000).decode())
