30995a4a11e50ae7e0ca3f09eaf161dc7fac3c76
wangzheng62 <363579018@qq.com>
2019-04-24 16:20:34 +0800

    11

--- SSHtool/classdef.py
+++ SSHtool/classdef.py
@@ -100,8 +100,31 @@ class Script():
         self.commandlist=tmp
 
 
-
+#initial
+def initial(server,commandlist):
+    sshclt = SSHClient()
+    sshclt.set_missing_host_key_policy(AutoAddPolicy())
+    sshclt.connect(hostname=server.hostname, port=server.port, username=server.username, password=server.password)
+    chan = sshclt.invoke_shell()
+    for com in commandlist:
+        chan.send(com)
+        chan.send('\n')
+        sleep(3)
+    tmp = chan.recv(1000)
+    print(tmp.decode())
 if __name__=='__main__':
+    # set root password. input user's password,new password,retype password
+    setpassword = ['sudo passwd root', '12345678', '12345678', '12345678']
+    # modify /root/.profile
+    modifyrootprofile = [r"sudo sed -i 's/mesg n/tty -s \&\& mesg n/g' /root/.profile", '12345678']
+    # modify ssh_config 16.04
+    modifysshconfig1604 = [
+        'sudo sed -i \'s/PermitRootLogin prohibit-password/PermitRootLogin yes/g\' /etc/ssh/sshd_config', '12345678',
+        'sudo service ssh restart']
+    # modify ssh_config 18.04
+    modifysshconfig1804 = ['sudo chmod u+w /etc/ssh/sshd_config', '12345678',
+                           r"sudo sed -i 's/\#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config",
+                           '12345678', 'sudo service ssh restart']
     server1={
         'hostname':'192.168.220.129',
         'port':22,
--- SSHtool/test.py
+++ SSHtool/test.py
@@ -4,20 +4,21 @@ from time import sleep
 #set root password. input user's password,new password,retype password
 setpassword=['sudo passwd root','12345678','12345678','12345678']
 #modify /root/.profile
-modifyrootprofile=['sudo sed \'s/mesg n/tty -s && mesg n/g\'','12345678']
-#modify ssh_config
-modifysshconfig=['sudo sed \'s/PermitRootLogin prohibit-password/PermitRootLogin yes/g\' /etc/ssh/sshd_config','12345678','sudo service ssh restart']
-#
+modifyrootprofile=[r"sudo sed -i 's/mesg n/tty -s \&\& mesg n/g' /root/.profile",'12345678']
+#modify ssh_config 16.04
+modifysshconfig1604=['sudo sed -i \'s/PermitRootLogin prohibit-password/PermitRootLogin yes/g\' /etc/ssh/sshd_config','12345678','sudo service ssh restart']
+#modify ssh_config 18.04
+modifysshconfig1804=['sudo chmod u+w /etc/ssh/sshd_config','12345678',r"sudo sed -i 's/\#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config",'12345678','sudo service ssh restart']
 sshclt=SSHClient()
 sshclt.set_missing_host_key_policy(AutoAddPolicy())
-sshclt.connect(hostname='192.168.220.129',port=22,username='wz',password='12345678')
+sshclt.connect(hostname='192.168.0.123',port=22,username='wz',password='12345678')
 chan=sshclt.invoke_shell()
-for com in setpassword:
-    chan.send(com+'\n')
+for com in modifysshconfig1804:
+    chan.send(com)
+    chan.send('\n')
     sleep(3)
-while(True):
-    tmp=chan.recv(1000)
-    print(tmp.decode())
+tmp=chan.recv(1000)
+print(tmp.decode())
 
 if __name__=='__main__':
     pass
