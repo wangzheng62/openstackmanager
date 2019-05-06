129490b481f40adf2dc7722cbf8a669c98bb87a1
wangzheng62 <363579018@qq.com>
2019-05-06 10:30:20 +0800

    11

--- SSHtool/oop.py
+++ SSHtool/oop.py
@@ -10,13 +10,35 @@ class Servernode():
         pass
     def applist(self):
         pass
+    def serverstatus(self):
+        pass
+
+class Bash(Servernode):
+    def sed(self):
+        pass
+    def apt(self):
+        pass
 #控制中心,接受服务器列表和待执行命令，将命令发送到每个服务器，并监控执行结果
 class Controlcenter():
     def __init__(self):
         pass
-    def Serverstatus(self):
+    def serverstatus(self):
         return {}
 #命令wrapper，将脚本，bash命令分解重组为规范命令
 class Commandwrapper():
     def __init__(self):
         pass
+#过程定义
+def getserver(*server,**serverlist):
+    #single ip
+    '192.168.0.1'
+    #
+    '192.168.0.1','192,168,0,2','192.168,0,3'
+    #
+    '192.168.0.1-25'
+    return ['192.168.0.1','192,168,0,2','192.168,0,3']
+def serverformat():
+    return {'hostname':'192.168.0.1',
+            'port':22,
+            'username':'root',
+            'password':'12345678'}
